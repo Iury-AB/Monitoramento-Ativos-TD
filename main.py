40a2f116a26935800db360b640ff3055fb3eaf3d
Iury-AB <iuryalves.iab@gmail.com>
2024-11-16 18:48:09 -0300

    first-final

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from random import sample
 import math
 from itertools import combinations
 from plot import plot_melhor_solucao
+import time
 
 
 np.set_printoptions(threshold=np.inf) # diretiva para imprimir todos os elementos de uma matriz
@@ -68,10 +69,12 @@ def fobj_1(x, probdata):
     f1 = soma(i:1->n) soma(j:1->m) [ x_ij * d_ij ]
     '''
     xyh=x.solution
-    for i in range(0, probdata.m):
-        for j in range(0, probdata.n):
+    n_equipes_base = 0
+    for j in range(0, probdata.n):
+        for i in range(0, probdata.m):
             if(xyh[i][j] != 0):
                 dist_soma +=  probdata.d[i][j]
+                n_equipes_base += 1
 
     x.fitness = dist_soma
 
@@ -202,10 +205,8 @@ def neighborhoodChange(x, y, k):
     if y.fitness < x.fitness:
         x = copy.deepcopy(y)
         k = 1
-        print("volta pra primeira vizinhanca\n")
     else:
         k += 1
-        print("proxima vizinhanca\n")
         
     return x, k
 
@@ -261,15 +262,7 @@ def shake(x, k, probdata):
     elif k == 2:           # altera aleatoriamente até duas equipes de bases
         y = troque_linha(x, y, probdata)        
         
-    elif k == 3: # altera aleatoriamente uma equipe de ativo e base
-        z = troque_linha(x, y, probdata)
-        y = copy.deepcopy(z)
-        y = troque_coluna(z, y, probdata)
-    elif k == 4: # poe 2 equipes na mesma base
-        pass
-    elif k == 5: # remove 2 equipe da mesma base
-        pass
-    elif k == 6:
+    elif k == 3:
         i_n0 = 0
         i_n1 = 0
         achou = False
@@ -289,6 +282,10 @@ def shake(x, k, probdata):
                         achou=True
                         y.solution[i_n0,n[1]] = x.solution[i_n0,n[0]]
                         y.solution[i_n1,n[1]] = 0
+    elif k == 4: # altera aleatoriamente uma equipe de ativo e base
+        z = troque_linha(x, y, probdata)
+        y = copy.deepcopy(z)
+        y = troque_coluna(z, y, probdata)
 
 
     return y
@@ -306,25 +303,26 @@ def vizinhanca(x, k, i):
         dupla = combinacao_base[i]
         y.solution[dupla[0],:] = x.solution[dupla[1],:]
         y.solution[dupla[1],:] = x.solution[dupla[0],:]
-        
-    elif k == 3: # altera aleatoriamente uma equipe de ativo e base
-        dupla = combinacao_ativo_base[i]
-        # Troca linhas diretamente
-        y.solution[dupla[1][0], :], y.solution[dupla[1][1], :] = (
-            y.solution[dupla[1][1], :].copy(),
-            y.solution[dupla[1][0], :].copy(),
-        )
+             
+    elif k == 3:
+        i_n0 = 0
+        i_n1 = 0
+        achou = False
+        while not achou:
+            dupla = combinacao_ativo[i]
+            for i,base in enumerate(x.bases_ocupadas):
+                if x.solution[base,dupla[0]]!=0 :
+                    i_n0 = base
+                  
+                if x.solution[base,dupla[1]]!=0:
+                    i_n1 = base
 
-        # Troca colunas diretamente
-        y.solution[:, dupla[0][0]], y.solution[:, dupla[0][1]] = (
-            y.solution[:, dupla[0][1]].copy(),
-            y.solution[:, dupla[0][0]].copy(),
-        )
-    elif k == 4: # poe 2 equipes na mesma base
-        pass
-    elif k == 5: # remove 2 equipe da mesma base
-        pass
-
+                if x.solution[i_n0,dupla[0]] != x.solution[i_n1,dupla[1]] and x.solution[i_n0,dupla[0]] !=0 and x.solution[i_n1,dupla[1]] !=0 :
+                    n0_carga = np.where(x.solution[i_n1]!=0)[0]
+                    if len(n0_carga) > x.resp - 1:
+                        achou=True
+                        y.solution[i_n0,dupla[1]] = x.solution[i_n0,dupla[0]]
+                        y.solution[i_n1,dupla[1]] = 0
 
     return y
 
@@ -335,7 +333,7 @@ def firstImprovement(x, obj, k, probdata):
     elif k == 2:
         tam_k = len(combinacao_base)
     elif k == 3:
-        tam_k = len(combinacao_ativo_base)
+        tam_k = len(combinacao_ativo)
 
     while (True):
         y=x
@@ -345,8 +343,11 @@ def firstImprovement(x, obj, k, probdata):
             xi = obj(xi, probdata)
             x = xi if (xi.fitness < x.fitness) else x
             i += 1 
+            #print("{}\n".format(i))
+            #print("{}\n".format(x.fitness))
 
         if(x.fitness >= y.fitness):
+            #print("{}".format(x.fitness))
             break
     return x
 
@@ -390,9 +391,11 @@ historico2.fit = []
 historico2.sol.append(x2.solution)
 historico2.fit.append(x2.fitness)
 
+tempo_inicio = time.time()
+tempo_timite = 60
 
 # Ciclo iterativo do método
-while num_sol_avaliadas < max_num_sol_avaliadas:
+while True:
     
     k = 1
     while k <= kmax:
@@ -402,16 +405,20 @@ while num_sol_avaliadas < max_num_sol_avaliadas:
         y = fobj_1(y,probdata)
         z = firstImprovement(y, fobj_1, k, probdata)
         z = fobj_1(z, probdata)
-        num_sol_avaliadas += 1
         
         # Atualiza solução corrente e estrutura de vizinhança (se necessário)
-        x,k = neighborhoodChange(x,y,k)
+        x,k = neighborhoodChange(x,z,k)
         
         # Armazena dados para plot
         historico.sol.append(x.solution)
         historico.fit.append(x.fitness)
+    
+    if(time.time() - tempo_inicio > tempo_timite):
+        break
 
-while num_sol2_avaliadas < max_num_sol_avaliadas:
+tempo_inicio = time.time()
+
+while True:
     
     k = 1
     while k <= kmax:
@@ -420,15 +427,18 @@ while num_sol2_avaliadas < max_num_sol_avaliadas:
         y2 = shake(x2,k,probdata)
         y2 = fobj_2(y2,probdata)
         z2 = firstImprovement(y2, fobj_2, k, probdata)
-        z2 = fobj_1(z2, probdata)
+        z2 = fobj_2(z2, probdata)
         num_sol2_avaliadas += 1
         
         # Atualiza solução corrente e estrutura de vizinhança (se necessário)
-        x2,k = neighborhoodChange(x2,y2,k)
+        x2,k = neighborhoodChange(x2,z2,k)
         
         # Armazena dados para plot
         historico2.sol.append(x2.solution)
         historico2.fit.append(x2.fitness)
+
+    if(time.time() - tempo_inicio > tempo_timite):
+        break
 
 print('\n--- SOLUÇÃO INICIAL CONSTRUÍDA ---\n')
 print('Sequência de tarefas atribuídas aos agentes:\n')
