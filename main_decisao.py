7ce2af32749d4e61fd32aa80a9ba0bad74852a64
Iury-AB <iuryalves.iab@gmail.com>
2025-01-15 15:58:14 -0300

    ahp

--- main_decisao.py
+++ main_decisao.py
@@ -94,7 +94,7 @@ def calcular_distancia_bases(coord_base1, coord_base2):
     distancia = geodesic(coord_base1, coord_base2).kilometers
     return distancia
 
-def robustez_indisponibilidade(solution, matriz_bases, probdata):
+def robustez_indisponibilidade(x, matriz_bases, probdata):
     """
     Avalia a qualidade de uma solução considerando a perturbação causada pela indisponibilidade de bases ocupadas.
     
@@ -112,11 +112,11 @@ def robustez_indisponibilidade(solution, matriz_bases, probdata):
     f2_min, f2_max = 412.6, 2119.8
 
     # Identificar as bases ocupadas
-    bases_ocupadas = set(np.where(solution.solution.sum(axis=1) > 0)[0])
+    bases_ocupadas = set(np.where(x.solution.sum(axis=1) > 0)[0])
 
     # Valores iniciais de f1 e f2
-    f1_original = fobj_1(solution, probdata).fitness
-    f2_original = fobj_2(solution, probdata).fitness
+    f1_original = fobj_1(x, probdata).fitness
+    f2_original = fobj_2(x, probdata).fitness
 
     perturbacoes_f1 = []
     perturbacoes_f2 = []
@@ -135,7 +135,7 @@ def robustez_indisponibilidade(solution, matriz_bases, probdata):
         base_mais_proxima = np.argmin(distancias)
 
         # Realocar as equipes e ativos para a base mais próxima
-        solution_modificada = solution
+        solution_modificada = x
         solution_modificada.solution[base_mais_proxima] += solution_modificada.solution[base_indisponivel]
         solution_modificada.solution[base_indisponivel] = 0
 
@@ -156,9 +156,109 @@ def robustez_indisponibilidade(solution, matriz_bases, probdata):
 
     # Calcular a norma euclidiana
     robustez = np.sqrt(media_perturbacao_f1 ** 2 + media_perturbacao_f2 ** 2)
-    solution.robustez = robustez
+    x.robustez = robustez
 
-    return solution
+    return x
+
+def balanco_carga(x, probdata):
+    carga_eq = np.zeros(3)
+    xyh = x.solution
+    for j in range(0, probdata.n):
+        for i in range(0, probdata.m):
+            if (xyh[i][j] == 1):
+                carga_eq[0] += 1
+                break
+            elif (xyh[i][j] == 2):
+                carga_eq[1] += 1
+                break
+            elif (xyh[i][j] == 3):
+                carga_eq[2] += 1
+                break
+    sd = np.std(carga_eq)
+    x.balanco_sd = sd
+    return x
+
+def ahp_classic(solucoes, matriz_comparacao_atributos):
+    """
+    Implementa o método AHP clássico para escolher entre soluções considerando quatro atributos.
+
+    Parâmetros:
+        solucoes (list): Lista de objetos, onde cada objeto tem os atributos `f1_value`, `f2_value`, `robustez`, `balanco_sd`.
+        matriz_comparacao_atributos (numpy.ndarray): Matriz 4x4 de comparação par a par entre os atributos.
+
+    Retorno:
+        Object: Solução escolhida após a execução do AHP.
+    """
+
+    def normalizar_matriz(matriz):
+        """Normaliza a matriz e retorna os pesos calculados."""
+        col_sum = np.sum(matriz, axis=0)
+        matriz_norm = matriz / col_sum
+        pesos = np.mean(matriz_norm, axis=1)
+        return pesos
+
+    # Cria as matrizes de comparação par a par para cada atributo
+    n = len(solucoes)
+    matrizes_atributos = { 
+        'f1_value': np.ones((n, n)), 
+        'f2_value': np.ones((n, n)), 
+        'robustez': np.ones((n, n)), 
+        'balanco_sd': np.ones((n, n)) 
+    }
+    
+    # Dicionário com os valores mínimos e máximos de cada atributo
+    limites = {
+        'f1_value': {'min': 1010.5, 'max': 1185.0},
+        'f2_value': {'min': 412.6, 'max': 427.5},
+        'robustez': {'min': 0, 'max': 0.03},
+        'balanco_sd': {'min': 0, 'max': 17.82},
+    }
+    
+    # Preenche as matrizes de comparação par a par com base nas diferenças normalizadas
+    for i in range(n):
+        for j in range(i + 1, n):  # Calcula apenas metade superior
+            for atributo in ['f1_value', 'f2_value', 'robustez', 'balanco_sd']:
+                val_i = getattr(solucoes[i], atributo)
+                val_j = getattr(solucoes[j], atributo)
+                
+                # Obtém os limites para o atributo atual
+                min_atributo = limites[atributo]['min']
+                max_atributo = limites[atributo]['max']
+                
+                # Normaliza a diferença com base nos limites do atributo
+                diff_normalizado = abs(val_i - val_j) / (max_atributo - min_atributo)
+                
+                # Normaliza a escala para 1 a 9
+                if diff_normalizado != 0:
+                    comparacao = min(max(round(1 + 8 * diff_normalizado, 2), 1), 9)
+                else:
+                    comparacao = 1  # Se os valores forem iguais
+                
+                # Preenche a matriz de comparações
+                if val_i < val_j:
+                    matrizes_atributos[atributo][i, j] = min(comparacao, 9)
+                else:
+                    matrizes_atributos[atributo][i, j] = min(round(1 / comparacao, 2), 9)
+                
+                matrizes_atributos[atributo][j, i] = min(round(1 / matrizes_atributos[atributo][i, j], 2), 9)
+
+    # Calcula os pesos das soluções em cada atributo
+    pesos_solucoes_por_atributo = {}
+    for atributo, matriz in matrizes_atributos.items():
+        pesos_solucoes_por_atributo[atributo] = normalizar_matriz(matriz)
+    
+    # Calcula os pesos dos atributos (prioridades) usando a matriz de comparação 4x4 fornecida
+    pesos_atributos = normalizar_matriz(matriz_comparacao_atributos)
+
+    # Calcula o peso final de cada solução
+    pesos_finais_solucoes = np.zeros(n)
+    for i in range(n):
+        for j, atributo in enumerate(['f1_value', 'f2_value', 'robustez', 'balanco_sd']):
+            pesos_finais_solucoes[i] += pesos_solucoes_por_atributo[atributo][i] * pesos_atributos[j]
+    
+    # Seleciona a solução com maior peso final
+    indice_melhor_solucao = np.argmax(pesos_finais_solucoes)
+    return (indice_melhor_solucao, solucoes[indice_melhor_solucao])
 
 probdata = probdef_new()
 
@@ -168,5 +268,8 @@ coord_bases = construir_matriz_bases("probdata.csv")
 
 for alternativa in alternativas:
     robustez_indisponibilidade(alternativa, coord_bases, probdata)
-    print(alternativa.robustez)
+    balanco_carga(alternativa, probdata)
 
+atributos_par_a_par = [[1, 3, 5, 1/2], [1/3, 1, 4, 3], [1/5, 1/4, 1, 7], [2, 1/3, 1/7, 1]]
+
+print(ahp_classic(alternativas, atributos_par_a_par)[0])
